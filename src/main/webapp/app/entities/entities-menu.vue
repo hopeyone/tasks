c1ffde73132dec5b111a5bc7e54c86a56f01d38b
Chris Hope <chris.hope@elysiumdigital.com.au>
2022-11-06 20:22:35 +1100

    fix es

--- src/main/webapp/app/entities/entities-menu.vue
+++ src/main/webapp/app/entities/entities-menu.vue
@@ -1,21 +1,5 @@
 <template>
   <div>
-    <b-dropdown-item to="/todo">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.todoTodo')">Todo</span>
-    </b-dropdown-item>
-    <b-dropdown-item to="/todo-group">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.todoTodoGroup')">Todo Group</span>
-    </b-dropdown-item>
-    <b-dropdown-item to="/todont">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.todontTodont')">Todont</span>
-    </b-dropdown-item>
-    <b-dropdown-item to="/todont-group">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.todontTodontGroup')">Todont Group</span>
-    </b-dropdown-item>
     <!-- jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here -->
   </div>
 </template>
--- src/main/webapp/app/entities/entities.component.ts
+++ src/main/webapp/app/entities/entities.component.ts
@@ -1,18 +1,10 @@
 import { Component, Provide, Vue } from 'vue-property-decorator';
 
 import UserService from '@/entities/user/user.service';
-import TodoService from './todo/todo/todo.service';
-import TodoGroupService from './todo/todo-group/todo-group.service';
-import TodontService from './todont/todont/todont.service';
-import TodontGroupService from './todont/todont-group/todont-group.service';
 // jhipster-needle-add-entity-service-to-entities-component-import - JHipster will import entities services here
 
 @Component
 export default class Entities extends Vue {
   @Provide('userService') private userService = () => new UserService();
-  @Provide('todoService') private todoService = () => new TodoService();
-  @Provide('todoGroupService') private todoGroupService = () => new TodoGroupService();
-  @Provide('todontService') private todontService = () => new TodontService();
-  @Provide('todontGroupService') private todontGroupService = () => new TodontGroupService();
   // jhipster-needle-add-entity-service-to-entities-component - JHipster will import entities services here
 }
